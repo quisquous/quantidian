50b1fb412f656add9d8426f691417a628ff23ecf
Adrienne Walker <enne@quisquo.us>
2013-05-19 20:46:34 -0700

    Bootstrap top level categories

--- client/quantidian.js
+++ client/quantidian.js
@@ -1,20 +1,33 @@
 Categories = new Meteor.Collection('categories');
 
-Template.categories.subscribed = function() {
-  var subscribedCategories = function() {
-    // FIXME: Need to add a user here and find the categories they care about.
-    var test = Categories.findOne({name: 'test'});
-    if (!test)
-      return [];
-    return [test._id];
-  };
+function subscribedCategoryIds() {
+  // FIXME: Need to add a user here and find the categories they care about.
+  var test = Categories.find({name: {$in: ['test', 'TestLetters']}});
+  return test.map(function(item) { return item._id; });
+}
 
+function subscribedCategories() {
   // FIXME: subscribedCategories is an ordered list.  Need to respect that
   // order through the map here.
-  var cursor = Categories.find({'_id': {$in: subscribedCategories()}});
+  var cursor = Categories.find({'_id': {$in: subscribedCategoryIds()}});
   return cursor.map(function(item) { return item; });
-};
+}
+
+Template.topLevelCategories.subscribed = subscribedCategories;
+Template.categories.subscribed = subscribedCategories;
+
+Template.topLevelCategories.events({
+  'click': function() {
+    Session.set("selectedCategory", this._id);
+  }
+});
 
 Template.category.typeIs = function(type) {
   return this.type === type;
 };
+
+Template.category.selectionClass = function() {
+  var selected = Session.equals("selectedCategory", this._id);
+  return selected ? "selected" : "unselected";
+};
+Template.topLevelCategory.selectionClass = Template.category.selectionClass;
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -13,7 +13,22 @@ Meteor.startup(function() {
         { desc: 'Fantastic', value: 5 },
       ],
     });
+    Categories.insert({
+      name: 'TestLetters',
+      longdesc: 'Pick the letter you feel best describes you.',
+      type: 'multiplechoice',
+      timestamp: (new Date()).getTime(),
+      choices: [
+        { desc: 'A', value: 'A' },
+        { desc: '\u03A0', value: '\u03A0' },
+        { desc: 'Q', value: 'Q' },
+        { desc: 'Z', value: 'Z' },
+      ],
+    });
   };
+
+  // FIXME: temporary auto-create entries
+  Categories.remove({});
 
   if (Categories.find().count() === 0) {
     initCategories();
