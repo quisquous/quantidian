5e0750d8f5803ca0eb3fb06453b0979bc9a20492
Adrienne Walker <enne@quisquo.us>
2013-05-27 17:28:04 -0700

    Modal confirmation dialog

--- client/quantidian.js
+++ client/quantidian.js
@@ -18,43 +18,15 @@ function subscribedCategories() {
   return Categories.find({'_id': {$in: subscribedCategoryIds()}}).fetch();
 }
 
-function logData(categoryId, value) {
-  var timestamp = new Date().getTime();
-  var datum = {
-    user_id: Meteor.userId(),
-    category_id: categoryId,
-    timestamp: timestamp,
-    created: timestamp,
-    modified: timestamp,
-    deleted: false,
-    comment: '',
-    value: value,
-  };
-  if (userLocation) {
-    datum.location = userLocation.coords;
-  }
-  Data.insert(datum);
-
-  // FIXME: Need some animation here about this getting recorded?
-}
-
 Template.main.subscribed = subscribedCategories;
 
 Template.category.typeIs = function(type) {
   return this.type === type;
 };
 
-Template.type_choice.events({
-  'click': function() {
-    Session.set("selectedChoice", this.value);
-    // FIXME: Add some large click-jacking animation that covers the page.
-    logData(Session.get("selectedCategory"), this.value);
-  },
-});
-
 Template.main.events({
   // FIXME: This really should be the Bootstrap 'shown' event.
-  'click': function(e) {
+  'click .categoryTab': function(e) {
     Session.set("selectedCategory", this._id);
   },
 });
@@ -75,20 +47,123 @@ Template.logEntries.recentLogs = function() {
 }
 
 Template.logEntries.stringifyLog = function() {
-  var category = Categories.findOne({'_id': this.category_id});
+  var category = categoryToString(this.category_id);
+  var value = valueToString(this.category_id, this.value);
+  var dateString = dateToString(this.created);
+  if (!category || !value)
+    return;
+  var commentString = "";
+  if (this.comment) {
+    commentString = ' "' + this.comment + '"';
+  }
+  return category + ": " + value + commentString + " (" + dateString + ")";
+}
+
+Template.typeChoice.events({
+  'click': function() {
+    var data = packData(this.value);
+    Session.set("confirmData", data);
+    $('#dataModal').modal('show');
+  },
+});
+
+Template.typeText.events({
+  'click .save': function(evt, template) {
+    var textElems = template.findAll('.text');
+    if (!textElems || !textElems[0].value)
+      return;
+    var data = packData(textElems[0].value);
+    Session.set("confirmData", data);
+    $('#dataModal').modal('show');
+  },
+});
+
+Template.dataModal.confirmData = function() {
+  var data = Session.get("confirmData");
+  // FIXME: If the #with confirmData is undefined, it doesn't react when set.
+  if (!data)
+    return {}
+  return data;
+};
+
+Template.dataModal.categoryName = function() {
+  return categoryToString(this.category_id);
+};
+
+Template.dataModal.valueString = function() {
+  return valueToString(this.category_id, this.value);
+};
+
+Template.dataModal.dateString = function() {
+  return dateToString(this.created);
+};
+
+Template.dataModal.locationString = function() {
+  if (!this.location)
+    return 'unknown';
+  return 'recorded';
+};
+
+Template.dataModal.events({
+  'click .accept': function() {
+    var commentElem = document.getElementById('modalComment');
+    if (commentElem) {
+      this.comment = commentElem.value;
+    }
+    // FIXME: validation
+    Data.insert(this);
+
+    $('#categoryTabs a[href="#maintab"]').tab('show')
+    $('#dataModal').modal('hide');
+    Session.set("confirmData", undefined);
+  },
+});
+
+function categoryToString(id) {
+  var category = Categories.findOne({'_id': id});
   if (!category)
-    return "error: couldn't find category";
-  var date = new Date(this.created);
-  var dateString = date.toDateString() + " " + date.toTimeString();
+    return "";
+  return category.name;
+}
+
+function valueToString(category_id, value) {
+  var category = Categories.findOne({'_id': category_id});
+  if (!category)
+    return "";
   if (category.type === "multiplechoice") {
-    var searchValue = this.value;
+    var searchValue = value;
     var choice = _.find(category.choices, function(choice) {
       return choice.value === searchValue;
     });
-    return category.name + ": " + choice.desc + " (" + dateString + ")";
+    return choice.desc;
   } else if (category.type == "text") {
-    return category.name + ": " + this.value + " (" + dateString + ")";
+    return value;
   } else {
-    return "error"
+    return "";
   }
+}
+
+function dateToString(timestamp) {
+  // FIXME: Do something smarter here like "5 minutes ago"
+  var date = new Date(timestamp);
+  return date.toDateString() + " " + date.toTimeString();
+}
+
+function packData(value) {
+  var categoryId = Session.get("selectedCategory");
+  var timestamp = new Date().getTime();
+  var data = {
+    user_id: Meteor.userId(),
+    category_id: categoryId,
+    timestamp: timestamp,
+    created: timestamp,
+    modified: timestamp,
+    deleted: false,
+    comment: '',
+    value: value,
+  };
+  if (userLocation) {
+    data.location = userLocation.coords;
+  }
+  return data;
 }
