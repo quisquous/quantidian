04c24ebd009cddf270fb08e4119887ce7a98bbb8
Adrienne Walker <enne@quisquo.us>
2013-07-30 20:55:03 -0700

    Add js/css for radio button groups

---
+++ client/radiobutton.js
@@ -0,0 +1,12 @@
+$(document).on('click.radiogroup', '.radioitem', function (e) {
+  if (!e.target) {
+    return;
+  }
+  var btn = $(e.target);
+  var group = btn.closest('.radiogroup')
+  if (!group) {
+    return;
+  }
+  group.find('.radioitem').removeClass('active');
+  btn.addClass('active');
+});
